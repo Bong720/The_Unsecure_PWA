2e9c1939a73bfc796194cc463c359186150d19cf
Bong720 <eric.bang4@education.nsw.gov.au>
2026-02-09 21:21:00 +0000

    gegege

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 import user_management as dbHandler
 
